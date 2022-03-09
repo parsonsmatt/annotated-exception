141c82b98ba78ce11913c860e0a1125ade4aac46
parsonsmatt <parsonsmatt@gmail.com>
2022-03-09 12:23:15 -0700

    Better type signatures

--- src/Control/Exception/Annotated/UnliftIO.hs
+++ src/Control/Exception/Annotated/UnliftIO.hs
@@ -1,3 +1,5 @@
+{-# language ExplicitForAll #-}
+
 -- | This module presents the same interface as
 -- "Control.Exception.Annotated", but uses 'MonadUnliftIO' instead of
 -- 'MonadCatch' or 'MonadThrow'.
@@ -58,20 +60,20 @@ import GHC.Stack
 --
 -- @since 0.1.2.0
 throwWithCallStack
-    :: (MonadIO m, Exception e, HasCallStack)
+    :: forall e m a. (MonadIO m, Exception e, HasCallStack)
     => e -> m a
 throwWithCallStack = liftIO . Catch.throwWithCallStack
 
 -- | Like 'Catch.throw', but uses 'MonadIO' instead of 'MonadThrow'.
 --
 -- @since 0.1.2.0
-throw :: (MonadIO m, Exception e) => e -> m a
+throw :: forall e m a. (MonadIO m, Exception e) => e -> m a
 throw = liftIO . Catch.throw
 
 -- | Like 'Catch.checkpoint', but uses 'MonadUnliftIO' instead of 'MonadCatch'.
 --
 -- @since 0.1.2.0
-checkpoint :: (MonadUnliftIO m) => Annotation -> m a -> m a
+checkpoint :: forall m a. (MonadUnliftIO m) => Annotation -> m a -> m a
 checkpoint ann action = withRunInIO $ \runInIO ->
     liftIO $ Catch.checkpoint ann (runInIO action)
 
@@ -79,7 +81,7 @@ checkpoint ann action = withRunInIO $ \runInIO ->
 -- 'MonadCatch'.
 --
 -- @since 0.1.2.0
-checkpointMany :: (MonadUnliftIO m) => [Annotation] -> m a -> m a
+checkpointMany :: forall m a. (MonadUnliftIO m) => [Annotation] -> m a -> m a
 checkpointMany anns action =
     withRunInIO $ \runInIO ->
         liftIO $ Catch.checkpointMany anns (runInIO action)
@@ -89,7 +91,7 @@ checkpointMany anns action =
 --
 -- @since 0.1.2.0
 checkpointCallStackWith
-    :: (MonadUnliftIO m, HasCallStack)
+    :: forall m a. (MonadUnliftIO m, HasCallStack)
     => [Annotation] -> m a -> m a
 checkpointCallStackWith anns action =
     withRunInIO $ \runInIO ->
@@ -99,7 +101,7 @@ checkpointCallStackWith anns action =
 --
 -- @since 0.1.2.0
 catch
-    :: (MonadUnliftIO m, Exception e)
+    :: forall e m a. (MonadUnliftIO m, Exception e)
     => m a
     -> (e -> m a)
     -> m a
@@ -111,7 +113,7 @@ catch action handler =
 --
 -- @since 0.1.2.0
 tryAnnotated
-    :: (MonadUnliftIO m, Exception e)
+    :: forall e m a. (MonadUnliftIO m, Exception e)
     => m a
     -> m (Either (AnnotatedException e) a)
 tryAnnotated action =
@@ -122,7 +124,7 @@ tryAnnotated action =
 --
 -- @since 0.1.2.0
 try
-    :: (MonadUnliftIO m, Exception e)
+    :: forall e m a. (MonadUnliftIO m, Exception e)
     => m a
     -> m (Either e a)
 try action =
@@ -133,7 +135,7 @@ try action =
 --
 -- @since 0.1.2.0
 catches
-    :: MonadUnliftIO m
+    :: forall m a. MonadUnliftIO m
     => m a
     -> [Handler m a]
     -> m a
